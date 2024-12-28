3ad924b1e3a1329acc957a2741b4df6d1fa7e544
Ahmet Hakan Hafif <59022934+colegion@users.noreply.github.com>
2024-12-28 12:21:36 +0300

    feat

---
+++ Assets/Scriptables/WeaponConfig.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Scriptables
+{
+    [CreateAssetMenu(fileName = "New Weapon Config", menuName = "DScriptableObjects/Weapon Config")]
+    public class WeaponConfig : ScriptableObject
+    {
+        
+    }
+}
